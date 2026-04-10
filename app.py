61545db7867ab4d790a6b257dfc3137070e502fa
anithamagam <168529686+anithamagam@users.noreply.github.com>
2026-04-10 16:45:58 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,7 +10,7 @@ from fastapi.responses import JSONResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.middleware.cors import CORSMiddleware
 
-from MachineLearningModule import MachineLearningFlow
+from MachineLearningModule.MachineLearningFlow import MachineLearningFlow
 from FeatureExtractionModule import FeatureExtraction
 
 app = FastAPI(title="Android Malware Detection API")
