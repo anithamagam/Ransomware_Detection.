eb14d78376bb31d56523f0f7db333fdd4c7c2b17
anithamagam <168529686+anithamagam@users.noreply.github.com>
2026-04-10 17:23:19 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,7 +1,12 @@
 from fastapi import FastAPI
+from fastapi.staticfiles import StaticFiles
+from fastapi.responses import FileResponse
 
 app = FastAPI()
 
+# Serve frontend folder
+app.mount("/static", StaticFiles(directory="frontend"), name="static")
+
 @app.get("/")
-def home():
-    return {"message": "Backend Running Successfully"}
+def serve_ui():
+    return FileResponse("frontend/index.html")
